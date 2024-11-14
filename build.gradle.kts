98e68844838aef3dd1695b935e6c0fdba81b1e60
Lyndon Gingerich <gingerichlyndon@gmail.com>
2024-11-13 23:14:42 -0600

    Add Cowsay library

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ dependencies {
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
+
+    implementation("com.github.ricksbrown:cowsay:1.1.0:lib")
 }
 
 compose.desktop {
